0632f6c587a2f7868f5c42d3ebec6bcf528c4a49
Zhiyan <goh.zhiyan@gmail.com>
2020-03-21 11:45:56 +0800

    added logger for MainController.java

--- src/main/java/learning/zhiyan/controller/MainController.java
+++ src/main/java/learning/zhiyan/controller/MainController.java
@@ -1,10 +1,14 @@
 package learning.zhiyan.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    private static Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/")
     public String welcomeMessage() {
